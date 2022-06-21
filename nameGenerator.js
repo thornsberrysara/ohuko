14e76597e65a8d65b72c961505f79d966407e4b3
thornsberrysara <thornsberrysara@gmail.com>
2022-06-21 15:28:46 -0400

    buggy but she works

--- nameGenerator.js
+++ nameGenerator.js
@@ -4,22 +4,75 @@ function vowelChar(length) {
   var vowelLength = vowel.length;
   for (var i = 0; i < length; i++) {
     result += vowel.charAt(Math.floor(Math.random() *
-    vowelLength));
+      vowelLength));
   }
   return result;
 }
 
 function consonantChar(length) {
   var result = '';
-  var consonant = 'bcdfghjklmnpqrstvwxyz';
+  var consonant = 'bcdfghjklmnprstvwxyz';
   var consonantLength = consonant.length;
   for (var i = 0; i < length; i++) {
     result += consonant.charAt(Math.floor(Math.random() *
-    consonantLength));
+      consonantLength));
   }
   return result;
 }
 
-var generateName = vowelChar(1) + consonantChar(1) + vowelChar(1) + consonantChar(1) + vowelChar(1)
+var generateName4 = consonantChar(1) + vowelChar(1) + consonantChar(1) + vowelChar(1)
+var generateName5 = vowelChar(1) + consonantChar(1) + vowelChar(1) + consonantChar(1) + vowelChar(1)
+var generateName6 = vowelChar(1) + consonantChar(1) + vowelChar(1) + consonantChar(1) + vowelChar(1) + consonantChar(1)
+var generateName7 = consonantChar(1) + vowelChar(1) + consonantChar(1) + vowelChar(1) + consonantChar(1) + vowelChar(1) + consonantChar(1)
 
-document.getElementById("name").innerHTML = generateName;
+function generateName(name) {
+  switch (name) {
+    case "four":
+      function displayFourCharacterName() {
+        const newH1 = document.createElement("h1")
+        const newContent = document.createTextNode(generateName4);
+        newH1.appendChild(newContent);
+        const currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newH1, currentDiv);
+      }
+
+      displayFourCharacterName()
+      break
+
+    case "five":
+      function displayFiveCharacterName() {
+        const newH1 = document.createElement("h1")
+        const newContent = document.createTextNode(generateName5);
+        newH1.appendChild(newContent);
+        const currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newH1, currentDiv);
+      }
+
+      displayFiveCharacterName()
+      break
+
+    case "six":
+      function displaySixCharacterName() {
+        const newH1 = document.createElement("h1")
+        const newContent = document.createTextNode(generateName6);
+        newH1.appendChild(newContent);
+        const currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newH1, currentDiv);
+      }
+
+      displaySixCharacterName()
+      break
+
+    case "seven":
+      function displaySevenCharacterName() {
+        const newH1 = document.createElement("h1")
+        const newContent = document.createTextNode(generateName7);
+        newH1.appendChild(newContent);
+        const currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newH1, currentDiv);
+      }
+
+      displaySevenCharacterName()
+      break
+  }
+}
